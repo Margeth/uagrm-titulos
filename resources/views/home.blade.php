3110cf428355fb1c0c20b5f4ee98a961921c9231
cubitos_magui <magui081191@gmail.com>
2022-07-28 00:03:38 -0400

    List Users

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -100,21 +100,19 @@
                     <table class="table table-striped table-sm">
                         <thead>
                         <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Header</th>
-                            <th scope="col">Header</th>
-                            <th scope="col">Header</th>
-                            <th scope="col">Header</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Tipo de Usuario</th>
+
                         </tr>
                         </thead>
                         <tbody>
                         @foreach($users as $user)
                         <tr>
-                            <td>{{ $user->id }}</td>
-                            <td>random</td>
-                            <td>data</td>
-                            <td>placeholder</td>
-                            <td>text</td>
+                            <td>{{ $user->name }}</td>
+                            <td>{{ $user->email }}</td>
+                            <td>{{ $user->name }}</td>
+
                         </tr>
                         @endforeach
 
